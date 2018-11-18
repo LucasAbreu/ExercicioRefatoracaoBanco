b3f4a05c9131be1242187f52c9bd3f786a360e60
LucasAbreu <lucasemabreu@gmail.com>
2018-11-18 17:49:52 -0200

    Implementado GerenciaContas. Todas chamadas de Conta são feitas por ela.

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/GerenciaContas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/GerenciaContas.java
@@ -1,15 +1,13 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
-
-import java.util.GregorianCalendar;
+ import java.util.ArrayList;
 import java.util.Map;
-
-public class GerenciaContas  {
-	private static GerenciaContas instance;
-	private Conta contaEmUso;
+ public class GerenciaContas  {
+	static GerenciaContas instance;
+	private int ContaEmUso;
 	private Map<Integer,Conta> listaContas;
-
-	private GerenciaContas() {
-		listaContas = Persistencia.getInstance().loadContas();
+	
+ 	private void Gerenciacontas() {
+		return;
 	}
 	
 	static public GerenciaContas getInstance() {
@@ -19,22 +17,68 @@ public class GerenciaContas  {
 		return instance;
 	}
 	
-	public void setContaEmUso(Integer nroConta) {
-		contaEmUso = listaContas.get(nroConta);
-	}
-	
-	public Conta getContaEmUso() {
-		return contaEmUso;
+	public void loadContas(Map<Integer,Conta> listaContas){
+		this.listaContas = listaContas;
 	}
 	
 	public Map<Integer,Conta> getListaContas(){
 		return this.listaContas;
 	}
 	
-	public void deposito(double valor) {
+	public void setContaEmUso(int conta) {
+		this.ContaEmUso = conta;
 	}
 	
-	public void retirada() {
-		
+	public Conta getContaEmUso() {
+		return listaContas.get(this.ContaEmUso);
 	}
+	
+	public Conta criaConta(int umNumero, String umNome,double umSaldo, int umStatus) throws Exception {
+		if(this.listaContas.get(umNumero) != null) {
+			throw new Exception("Numero de conta já existe");
+		}
+		Conta c = new Conta(umNumero, umNome, umSaldo, umStatus);
+		this.listaContas.put(umNumero, c);
+		return c;
+	}
+	
+	public void apagaConta(int conta) {
+		this.listaContas.remove(conta);
+	}
+	
+	public double deposito(double valor){
+		this.listaContas.get(this.ContaEmUso).deposito(valor);
+		return this.listaContas.get(this.ContaEmUso).getSaldo();
+	}
+	
+	public double retirada(double valor) {
+		this.listaContas.get(this.ContaEmUso).retirada(valor);
+		return this.listaContas.get(this.ContaEmUso).getSaldo();
+	}
+	
+	public double getSaldo() {
+		return this.listaContas.get(this.ContaEmUso).getSaldo();
+	}
+	
+	public String getCorrentista() {
+		return this.listaContas.get(this.ContaEmUso).getCorrentista();
+	}
+	
+	public int getStatus() {
+		return this.listaContas.get(this.ContaEmUso).getStatus();
+	}
+	
+	public String getStrStatus() {
+		return this.listaContas.get(this.ContaEmUso).getStrStatus();
+	}
+	
+	public double getLimRetiradaDiaria() {
+		return this.listaContas.get(this.ContaEmUso).getLimRetiradaDiaria();
+	}
+	
+	public String toString() {
+		return this.listaContas.get(this.ContaEmUso).toString();
+	}
+	
+	
 }
