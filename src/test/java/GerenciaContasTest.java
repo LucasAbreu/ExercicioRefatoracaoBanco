659064cc759d689580e1c2139a0bb8b9a2e188e8
hramires <henrique.ramires@acad.pucrs.br>
2018-11-27 18:56:09 -0200

    classes test

---
+++ src/test/java/GerenciaContasTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+
+class GerenciaContasTest {
+	private GerenciaContasTest gerenciaContasTest;
+
+	@BeforeEach
+	public void inicializa() {
+		gerenciaContasTest = new GerenciaContasTest();
+	}
+
+}
---
+++ src/test/java/GerenciadorOperacoesTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.jupiter.api.BeforeEach;
+
+class GerenciadorOperacoesTest {
+	private GerenciadorOperacoesTest gerenciadorOperacoesTest;
+
+	@BeforeEach
+	public void inicializa() {
+		gerenciadorOperacoesTest = new GerenciadorOperacoesTest();
+	}
+
+}
