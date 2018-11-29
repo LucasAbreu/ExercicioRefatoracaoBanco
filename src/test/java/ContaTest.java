24e9e9a40d89b1d5a2aa2b53b62465057a025c9b
LucasAbreu <lucasemabreu@gmail.com>
2018-11-28 23:29:31 -0200

    Corrigido erro nos testes conta.

--- src/test/java/ContaTest.java
+++ src/test/java/ContaTest.java
@@ -25,48 +25,32 @@ class ContaTest {
 		assertEquals(conta.getSaldo(), sf);
 	}
 
-	
+
 	@DisplayName("Testa mudança de subida de status")
 	@ParameterizedTest
 	@CsvSource({"0, 10, 0, 0",	// Silver -> Silver
-			"0, 49999, 0, 1",	// Silver -> Gold
-			"0, 500000, 0, 2"	// Silver -> Gold -> Plat
-			})
+		"0, 49999, 0, 1",	// Silver -> Gold
+		"0, 500000, 0, 2"	// Silver -> Gold -> Plat
+	})
 	public void testeDepositoStatusUp(double saldo, double valorDepositado, int stateInit, int stateFinal) {
-		GerenciaContas gc = GerenciaContas.getInstance();
-		try {
-			gc.apagaConta(1);
-			gc.criaConta(1, "Fulano", saldo, stateInit);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		gc.setContaEmUso(1);
-		gc.deposito(valorDepositado);
-		gc.deposito(1);
-		assertEquals(stateFinal, GerenciaContas.getInstance().getStatus());
+		Conta c = new Conta(1, "Fulano", saldo, stateInit);
+		c.deposito(valorDepositado);
+		c.deposito(1);
+		assertEquals(stateFinal, c.getStatus());
 	}
-	
+
 	@DisplayName("Testa mudança de descida de status")
 	@ParameterizedTest
 	@CsvSource({"101000, 10000,  2, 1",	// Plat -> Gold
-			"101000, 80000, 2, 0"		// Plat -> Silver
-			})
+		"101000, 80000, 2, 0"		// Plat -> Silver
+	})
 	public void testeRetiradaStatusDown(double saldo, double valorRetirado, int stateInit, int stateFinal) {
-		GerenciaContas gc = GerenciaContas.getInstance();
-		try {
-			gc.apagaConta(1);
-			gc.criaConta(1, "Fulano", saldo, stateInit);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		gc.setContaEmUso(1);
-		gc.retirada(valorRetirado);
-		gc.retirada(1);
-		assertEquals(stateFinal, GerenciaContas.getInstance().getStatus());
+		Conta c = new Conta(1, "Fulano", saldo, stateInit);
+		c.retirada(valorRetirado);
+		c.retirada(1);
+		assertEquals(stateFinal, c.getStatus());
 	}
-	
+
 	@DisplayName("Testa saque")
 	@ParameterizedTest
 	@CsvSource({ "10, 5, 5, 0, 0" })
@@ -78,6 +62,6 @@ class ContaTest {
 		assertEquals(conta.getSaldo(), sf);
 	}
 
-	
-	
+
+
 }
